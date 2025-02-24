80857513b210579e418b2b78e7845c0b036ac71a
JamesVictor-o <victorjames40v@gmail.com>
2025-02-24 22:50:24 +0100

    made changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import TrendingGaming from "@/components/TrendingGaming";
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#0D1216]">
-      {/*Import and render the LandingPage component here */}
+        <TrendingGaming/>
     </div>
   );
 }
