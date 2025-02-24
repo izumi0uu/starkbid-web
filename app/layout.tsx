c2923f61f6ef3c8ea3b1c1ad957080ecb4fa259e
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2025-02-24 18:41:26 +0100

    fix: moved the footer and navbar the component folder in the root

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "../components/navbar";
-import Footer from "../components/footer";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +33,7 @@ export default function RootLayout({
         <div className="flex flex-col">
           <Navbar />
           <main className="flex-grow">{children}</main>
-          <Footer />
+          <Footer/>
         </div>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-[#0D1216]">
       {/*Import and render the LandingPage component here */}
     </div>
   );
