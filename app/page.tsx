8ff12fd5cb13ed82c14b5a250b81799eeda3c50c
JamesVictor-o <victorjames40v@gmail.com>
2025-02-25 18:26:50 +0100

    implement the trending game section

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#0D1216]">
-        <TrendingGaming/>
       <main className="">
         <Hero />
+        <TrendingGaming/>
       </main>
     </div>
   );
