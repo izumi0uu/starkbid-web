ed18edb9f9047f603b5bf87eacd03ffac566f862
Nwanne Nnamani <nnamaninwanne@gmail.com>
2025-02-26 18:56:12 +0100

    feat: implement nft collection section, build connect wallet modal for navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import NFTCollectionsTable from "@/components/Collections";
 import Hero from "@/components/Hero";
 import Newsletter from "@/components/Newsletter";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#0D1216]">
-      <main className="">
+      <main className="md:space-y-[100px]">
         <Hero />
+        <NFTCollectionsTable/>
         <Newsletter />
       </main>
     </div>
