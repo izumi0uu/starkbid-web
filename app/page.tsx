6269e26196437ed532cb57b96cae6bdd25658395
ebubechi-ihediwa <ebubechiihediwa@gmail.com>
2025-02-26 01:35:30 +0100

    feat: Implement NFT Category Filter & Featured Collection Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
+import NFTCategory from "@/components/nft-category";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#0D1216]">
       <main className="">
         <Hero />
+        <NFTCategory />
       </main>
     </div>
   );
