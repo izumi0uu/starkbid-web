c8b5701f7391cac71aa86651f79526b7e5192756
ebubechi-ihediwa <ebubechiihediwa@gmail.com>
2025-02-27 11:38:51 +0100

    Modified nft category button animation

--- components/nft-category.tsx
+++ components/nft-category.tsx
@@ -23,9 +23,8 @@ const NFTCategory = () => {
                     return (
                         <motion.div
                             key={value}
-                            initial={{ opacity: 0, y: 10 }}
-                            animate={{ opacity: 1, y: 0 }}
-                            whileHover={{ scale: 1.1 }}
+        
+                            whileHover={{ scale: 0.9 }}
                             transition={{ duration: 0.5, ease: "easeInOut" }}
                             onClick={() => setActiveTab(value)}
                             role="button"
