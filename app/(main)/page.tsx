f1a5a1fccce4ba72a9371c5ca833c651a265d5a4
davedumto <starkcare01@gmail.com>
2025-03-24 04:53:19 +0100

    feat: built the trending nft section

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,11 @@
-
+import TrendingNft from '@/components/landing-page/trending-nft'
 
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[#0D1216]">
+    <div className="min-h-screen bg-black">
       <main className=" flex flex-col gap-20 px-4 sm">
-        
+        <TrendingNft/>
       </main>
     </div>
   );
