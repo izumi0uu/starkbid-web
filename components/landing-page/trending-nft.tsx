07631b03e3772c92c929015e3e42e39fb9ad2b31
AnnetteCodes <starkcare01@gmail.com>
2025-03-25 19:25:04 +0100

    chore:addressed comment on pr

--- components/landing-page/trending-nft.tsx
+++ components/landing-page/trending-nft.tsx
@@ -4,20 +4,7 @@ import Image from 'next/image';
 import { BsHeart, BsHeartFill } from 'react-icons/bs';
 import { MdVerified } from 'react-icons/md';
 import Arrow from '@/public/arrow.png'
-
-
-interface NFTCardProps {
-  id: number;
-  title: string;
-  creatorName: string;
-  creatorIcon: string;
-  isVerified: boolean;
-  currentBid: number;
-  likes: number;
-  minted: number;
-  timeLeft: string;
-  image: string;
-}
+import { NFTCardProps, nftData } from '@/constants/data';
 
 const NFTCard: React.FC<NFTCardProps> = ({
   title,
@@ -68,7 +55,7 @@ const NFTCard: React.FC<NFTCardProps> = ({
           src={`/${image}`} 
           alt={title}
           fill
-          className={`px-4 transition-all duration-300 ease-in-out ${isHovered ? 'scale-[1.03] blur-[2px]' : ''}`}
+          className={`px-4 transition-all duration-300 ease-in-out ${isHovered ? 'scale-[1.03] ' : ''}`}
         />
         {isHovered && (
           <button className="absolute bottom-20 left-1/2 transform -translate-x-1/2 border border-gray-400 bg-white text-black px-4 py-2 rounded-md text-sm font-medium z-10 transition-all duration-300 opacity-100 shadow-md hover:bg-gray-100">
@@ -97,109 +84,9 @@ const NFTCard: React.FC<NFTCardProps> = ({
 
 const TrendingNFTs: React.FC = () => {
   const [activeTab, setActiveTab] = useState('trending');
-  
-  
-  const nftData: NFTCardProps[] = [
-    {
-      id: 1,
-      title: "The Pink-Blackened Horned Knight",
-      creatorName: "Jacksonito",
-      creatorIcon: "icon1.png",
-      isVerified: true,
-      currentBid: 0.956,
-      likes: 564,
-      minted: 24,
-      timeLeft: "1d 9h left",
-      image: "1.png"
-    },
-    {
-      id: 2,
-      title: "Relax & Breath",
-      creatorName: "Bitogadick",
-      creatorIcon: "icon2.png",
-      isVerified: false,
-      currentBid: 0.956,
-      likes: 564,
-      minted: 24,
-      timeLeft: "1d 9h left",
-      image: "2.png"
-    },
-    {
-      id: 3,
-      title: "Running Time",
-      creatorName: "Mickyvandy",
-      creatorIcon: "icon3.png",
-      isVerified: true,
-      currentBid: 0.745,
-      likes: 24,
-      minted: 2,
-      timeLeft: "4d 9h left",
-      image: "3.png"
-    },
-    {
-      id: 4,
-      title: "Yellow Weather",
-      creatorName: "Karhty26",
-      creatorIcon: "icon4.png",
-      isVerified: true,
-      currentBid: 0.956,
-      likes: 564,
-      minted: 24,
-      timeLeft: "1d 9h left",
-      image: "4.png"
-    },
-    {
-      id: 5,
-      title: "Space 101",
-      creatorName: "Fame Static",
-      creatorIcon: "icon5.png",
-      isVerified: true,
-      currentBid: 0.956,
-      likes: 564,
-      minted: 24,
-      timeLeft: "1d 9h left",
-      image: "5.png"
-    },
-    {
-      id: 6,
-      title: "Cool Fever",
-      creatorName: "Ella Walker",
-      creatorIcon: "icon6.png",
-      isVerified: true,
-      currentBid: 0.956,
-      likes: 564,
-      minted: 24,
-      timeLeft: "1d 9h left",
-      image: "6.png"
-    },
-    {
-      id: 7,
-      title: "Mood Of Staces",
-      creatorName: "Hades KM",
-      creatorIcon: "icon7.png",
-      isVerified: true,
-      currentBid: 0.956,
-      likes: 564,
-      minted: 24,
-      timeLeft: "1d 9h left",
-      image: "7.png"
-    },
-    {
-      id: 8,
-      title: "The Seal Man",
-      creatorName: "Olushola",
-      creatorIcon: "icon8.png",
-      isVerified: true,
-      currentBid: 0.956,
-      likes: 564,
-      minted: 24,
-      timeLeft: "1d 9h left",
-      image: "8.png"
-    },
-  ];
 
   return (
-    <div className="bg-black text-white p-6">
+    <div className="bg-black text-white p-6 max-w-8xl mx-auto">
       <div className="flex gap-[1em] mb-8">
         <button 
           className={`px-2 py-1 ${activeTab === 'trending' ? 'text-white font-bold text-[20px] bg-[#1C1D1F] px-4 py-2 rounded-md' : 'text-gray-500 text-[20px]'}`}
