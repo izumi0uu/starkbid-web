287a74c81eff95d8c87eb1fa6d46b9e538dcaac5
drakkomaximo <andres.villanueva.tabares@gmail.com>
2025-07-06 19:08:53 -0500

    Build issues have been solved

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,6 +2,7 @@ import ProfileHeader from "@/components/profile/profile-header";
 import Footer from "@/components/collections/footer";
 import Navbar from "@/components/landing-page/Navbar";
 import CollectionTabs from "@/components/profile/profile-collections";
+import { Suspense } from "react";
 
 export default function Page() {
 
@@ -10,7 +11,9 @@ export default function Page() {
       <Navbar />
       <div className="flex-1 w-full text-white overflow-hidden">
         <ProfileHeader />
-        <CollectionTabs />
+        <Suspense fallback={<div className="p-6">Loading...</div>}>
+          <CollectionTabs />
+        </Suspense>
       </div>
       <Footer />
     </div>
