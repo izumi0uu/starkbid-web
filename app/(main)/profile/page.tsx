80850d9c202369b61acd3ea3238e75a179c38a37
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2025-07-23 20:15:19 +0100

    chore: add activity component to profile page

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,16 +1,12 @@
 import ProfileHeader from "@/components/profile/profile-header";
 import CollectionTabs from "@/components/profile/profile-collections";
-import { Suspense } from "react";
 
 export default function Page() {
-
   return (
     <div className="min-h-screen flex flex-col bg-[#101213]">
       <div className="flex-1 w-full text-white overflow-hidden">
         <ProfileHeader />
-        <Suspense fallback={<div className="p-6">Loading...</div>}>
-          <CollectionTabs />
-        </Suspense>
+        <CollectionTabs />
       </div>
     </div>
   );
